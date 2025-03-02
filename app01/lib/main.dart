3ce7456eb075b9fc529ae302dcc7e80283146b5a
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-02 10:46:09 +0530

    add  safearea

--- app01/lib/main.dart
+++ app01/lib/main.dart
@@ -11,35 +11,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xff000080),
-
-          title: Text('App Bar',
-            style: TextStyle(
+       home: SafeArea(
+         child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xff000080),
+         
+            title: Text('App Bar',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+         
+            leading: Icon(
+              Icons.menu,
               color: Colors.white,
-              fontWeight: FontWeight.w500,
+              size: 35,
             ),
+         
+            actions: [Icon(
+              Icons.account_circle,
+              color: Colors.white,
+              size: 35,),
+            ],
           ),
-
-          leading: Icon(
-            Icons.menu,
-            color: Colors.white,
-            size: 35,
-          ),
-
-          actions: [Icon(
-            Icons.account_circle,
-            color: Colors.white,
-            size: 35,),
-          ],
-        ),
-
-        body: Center(
-          child: Text('Hello Flutter',
-          style: TextStyle(fontSize: 30),
+         
+          body: Center(
+            child: Text('Hello Flutter',
+            style: TextStyle(fontSize: 30),
+              ),
             ),
-          ),
+         ),
        ),
     );
   }
